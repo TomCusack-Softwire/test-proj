63c66ed47eba2f6db9f82c0c348317e712a70cf3
Tom Cusack <104017547+TomCusack-Softwire@users.noreply.github.com>
2023-05-15 16:56:51 +0100

    Add GitHub Action to check linting (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 export default function Home() {
-    return <>
-        <h1> Hello, world! </h1>
+    return <><h1> Hello, world! </h1>
         <p> This is a very basic web page. </p>
     </>;
 }
