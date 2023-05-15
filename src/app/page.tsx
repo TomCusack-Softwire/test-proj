89a1322df70f5737e7895133fe7da53f53f97322
TomCusack-Softwire <Tom.Cusack@softwire.com>
2023-05-15 16:41:06 +0100

    test1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 export default function Home() {
-    return <>
-        <h1> Hello, world! </h1>
-        <p> This is a very basic web page. </p>
+    return <><h1> Hello, world! </h1>
+<p> This is a very basic web page. </p>
     </>;
 }
